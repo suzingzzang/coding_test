03e2f596428c6053ddbcc00c2459059ef7dbaa2c
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-06 11:53:50 +0900

    level_2_푸는중

---
+++ 배달.py
@@ -0,0 +1,10 @@
+def solution(N, road, K):
+    answer = 0
+    distance = [] * (N+1)
+    for j in road[N]:
+        print(j)
+        #distance[j[0]] = j[1]
+        #print(j[0])
+
+
+    return answer
