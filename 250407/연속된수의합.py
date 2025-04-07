8c8d82875480bcd830e8bbe292ee61cffadaa4f0
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-08 08:19:03 +0900

    level_0_이해안됨

--- 250407/연속된수의합.py
+++ 250407/연속된수의합.py
@@ -7,3 +7,21 @@ def solution(num, total):
         sets_i = set()
         print(set_i)
     return answer
+
+
+##################################
+# 8~11라인 이해안됨
+def solution(num, total):
+    
+    answer = []
+    d = 0
+    
+    for i in range(1, num):
+        d += i
+        
+    a1 = (total - d) // num
+    
+    for i in range(a1, a1 + num):
+        answer.append(i)
+    
+    return answer
