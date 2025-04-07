8f5641148018a06a01e965dfb0d8299669d593a9
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-08 08:23:25 +0900

    level_0_이해안됨

---
+++ 250407/특이한정렬.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120880
+
+def solution(numlist, n):
+    numlist.sort(key=lambda x: (abs(x - n), -x))
+    # x를 abs(x - n) 값에 따라 정렬. 동일할 경우 -x를 기준으로 정렬 
+    # 즉, 본래 x의 값이 클수록 우선순위가 높다. 
+    answer = []
+    for i in numlist:
+        answer.append(i)
+    return answer
