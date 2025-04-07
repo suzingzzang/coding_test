7011ccea2e9e829fd48d1ac59972a72a6ab27e05
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-07 12:56:29 +0900

    level_0

---
+++ 250407/다음에올숫자.py
@@ -0,0 +1,9 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120924
+
+def solution(common):
+    answer = 0
+    if common[1] - common[0] == common[2] - common[1]:
+        answer = common[-1]+(common[1]-common[0])
+    elif common[1] // common[0] == common[2] //common[1]:
+        answer = common[-1] * (common[1] // common[0])
+    return answer
