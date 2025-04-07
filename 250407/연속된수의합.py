3c3dc91ccbb14eed9a1b0b1ce4e5f14145a8f590
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-07 20:54:04 +0900

    level_0_이해안됨...

---
+++ 250407/연속된수의합.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120923
+
+def solution(num, total):
+    answer = []
+    sets = set()
+    for i in range(num):
+        sets_i = set()
+        print(set_i)
+    return answer
