e81f74837585cd95496cd54d2ea90014e18d698e
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-08 09:42:00 +0900

    level_0_막힘

---
+++ 250407/문자열밀기.py
@@ -0,0 +1,11 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120921
+
+def solution(A, B):
+    a = []
+    b = []
+    for i in range(len(A)):
+        a.append(ord(A[i]))
+        b.append(ord(B[i]))
+    
+          
+    return a
