ae22521008407a5fb1cee60e1af3e4edaa81f416
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-09 15:48:07 +0900

    retry_success

--- 250407/문자열밀기.py
+++ 250407/문자열밀기.py
@@ -9,3 +9,17 @@ def solution(A, B):
     
           
     return a
+###############################################################
+from collections import deque
+
+def solution(A, B):
+    
+    deque_A = deque(A)
+    deque_B = deque(B)
+    
+    for i in range(len(deque_A)):
+        if deque_A == deque_B:
+            return i
+        deque_A.rotate(1)
+        
+    return -1
