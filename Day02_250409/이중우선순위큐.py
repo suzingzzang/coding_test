5a16fe1442b85321d183a497e34530533c9424e2
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-10 15:37:32 +0900

    힙_level_3_테케7통과안됨

---
+++ Day02_250409/이중우선순위큐.py
@@ -0,0 +1,22 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+import heapq
+def solution(operations):
+    answer = []
+    heap = []
+    for i in operations:
+        num = int(i.split(' ')[1])
+        if 'I' in i:
+            heapq.heappush(heap,num)            
+        elif 'D' in i:
+            if heap:
+                if num == -1:
+                    heapq.heappop(heap)
+                elif num == 1:
+                    heap.pop()
+
+                
+    if heap:
+        return [heapq.nlargest(1,heap)[0],heap[0]]
+    else:
+        return [0,0]
