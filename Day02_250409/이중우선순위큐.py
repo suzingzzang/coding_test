5c5a38da139017ed80a252b99b55f4921fb1b912
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-10 22:07:50 +0900

    힙_level_3

--- Day02_250409/이중우선순위큐.py
+++ Day02_250409/이중우선순위큐.py
@@ -20,3 +20,24 @@ def solution(operations):
         return [heapq.nlargest(1,heap)[0],heap[0]]
     else:
         return [0,0]
+######################################################################    min_heap / max_heap 으로도 풀기 
+import heapq
+def solution(operations):
+    answer = []
+    heap = []
+    for i in operations:
+        num = int(i.split(' ')[1])
+        if 'I' in i:
+            heapq.heappush(heap,num)            
+        elif 'D' in i:
+            if heap:
+                if num == -1:
+                    heapq.heappop(heap)
+                elif num == 1:
+                    heap.remove(heapq.nlargest(1,heap)[0])
+
+                
+    if heap:
+        return [heapq.nlargest(1,heap)[0],heap[0]]
+    else:
+        return [0,0]
