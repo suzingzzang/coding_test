f0231e121252c39d2186541d6f7000ce7d19be47
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-15 11:04:37 +0900

    질문

---
+++ Day04_250413/최소직사각형.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+def solution(sizes):
+    
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
+
+for i in sizes:
+  max(i)
