07b6aa6a7706d5b50b4c0227c1fb61d036cb7400
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-21 09:22:30 +0900

    그리디

---
+++ Day05_250417/큰수만들기.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+
+def solution(number, k):
+    answer = ''
+    stack = []
+    for i in number:
+        while k > 0 and  stack and stack[-1] < i:
+            stack.pop()
+            k -= 1
+            
+        stack.append(i)
+    
+    if k>0:
+        stack = stack[:-k]
+    
+    return ''.join(stack)
