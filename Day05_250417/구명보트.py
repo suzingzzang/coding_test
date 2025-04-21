e82440c1fb50d8dc3f2d3d8ce2f06d8e5506d0f1
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-04-21 10:03:33 +0900

    그

---
+++ Day05_250417/구명보트.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42885
+
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    light , heavy = 0,len(people) -1
+    
+    while light <= heavy:
+        if people[light] + people[heavy] <= limit:
+            answer += 1
+            light +=1
+            heavy -= 1
+        else:
+            heavy -= 1
+            answer +=1
+            
+    return answer
