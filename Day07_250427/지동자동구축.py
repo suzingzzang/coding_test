42116ea84cf5e16b6170ed6047cb29ccee875b8e
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-05-01 16:07:31 +0900

    softeer_lv2

---
+++ Day07_250427/지동자동구축.py
@@ -0,0 +1,7 @@
+# https://softeer.ai/app/assessment/index.html?xid=462560&xsrfToken=akQziR5BDvT4FBqQBqTT4kZMZgFCpNh2&testType=practice
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+print((2**N + 1)**2)
