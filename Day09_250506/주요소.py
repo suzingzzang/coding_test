99a91e8adc6e0b80b356c6179da85f62a0bd6d70
Sujin Ji <122340036+suzingzzang@users.noreply.github.com>
2025-05-08 13:55:34 +0900

    백준_실버3

---
+++ Day09_250506/주요소.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/13305
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+rodes = list(map(int,input().split()))
+prices = list(map(int,input().split()))
+
+min_price = prices[0]
+total_cost = 0
+for i in range(N-1):
+    if prices[i] < min_price:
+        min_price = prices[i]
+    total_cost += min_price*rodes[i]
+print(total_cost)
